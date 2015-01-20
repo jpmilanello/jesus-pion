5831b144e8c871e21f44633b88458a3c91d94705
jpmilanello <jesuspiont@gmail.com>
2015-01-20 14:24:46 -0500

    add image pop up

--- public/js/views/section-works/list.js
+++ public/js/views/section-works/list.js
@@ -5,11 +5,12 @@ define([
   // Using the Require.js text! plugin, we are loaded raw text
   // which will be used as our views primary template
   'collections/works',
-  'text!templates/section-works/list.html'
-], function($, _, Backbone, Works,workListTemplate){
+  'text!templates/section-works/list.html',
+  'text!templates/section-works/hidden.html',
+], function($, _, Backbone, Works,workListTemplate,hiddenListTemplate){
   	var SectionWorkList = Backbone.View.extend({
   		/* HTML element for single page app */
-  		el: '.list-works',
+  		el: '#section-works',
   		render: function () {
   			/* Create an instance for reference in anonymous function */
   			var element = this;
@@ -20,7 +21,9 @@ define([
   				success: function (works) {
   					/* Load the html template with the data obtained. Each row is passed in JSON format */
   					var listTemplate = _.template(workListTemplate)({works: works.models});
-  					element.$el.html(listTemplate);
+  					element.$('.list-works').html(listTemplate);
+            var hiddenTemplate = _.template(hiddenListTemplate)({works: works.models});
+            element.$('.hiddenDivs').html(hiddenTemplate);
             var documentWidth = $(document).width();
             $('#backward').addClass('disabled');
             $('.work-article:first-child').addClass('col-sm-offset-2 col-xs-offset-3');
@@ -34,6 +37,21 @@ define([
   				}
   			})		
   		},
+      events: {
+        'click .smallImg': 'openImage',
+        'click .close-img': 'closeImage'
+      },
+      openImage: function (ev) {
+        //show full image
+        var element = $(ev.currentTarget);
+        idImg = element.attr('id').split("img")[1];
+        $('.bigImage#BI'+idImg).removeClass('hide');
+      }, 
+      closeImage: function (ev) {
+        //show full image
+        var element = $(ev.currentTarget);
+        element.parent().addClass('hide');
+      } 
   	});
   // Our module now returns our view
   return SectionWorkList;
